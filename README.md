5f02010684132ca63ee3e78128c07d48f7cabee7
burnedsap <salil458@gmail.com>
2021-02-08 22:33:19 -0500

    v2 functional

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # pixelcryption
 👀👀👀
+<<<<<<< Updated upstream
 https://burnedsap.github.io/pixelcryption/
+=======
+
+A simple and easy to use encryption and decryption tool. Use this to encrypt messages within images.
+
+
+
+v1 stable version: http://burnedsap.github.io/pixelcryption/v1/
+
+**v1 Features**
+
+- Pixel level encryption
+- Simple interface
+- Max encryption length: 280 chars (including spaces)
+
+
+
+v2 currently in development, see beta here: http://burnedsap.github.io/pixelcryption/
+
+
+
+**v2 Features**
+
+- Pixel Level Encryption
+- Custom cypher (max length 255 chars)
+- Open-source
+
+
+
+**To Do List**
+
+- [x] Custom encryption message length
+- [ ] Identify length of message to be decrypted
+- [x] Create randomised and custom cyphers
+- [ ] New visual design
+- [ ] Add additional levels of encryption prior to image encryption
+- [ ] Offline/downloadable version
+
+>>>>>>> Stashed changes
--- sketch.js
+++ sketch.js
@@ -1,203 +1,190 @@
-/*
-Attribution
-textarea: @lmccart (Github) @kanolab (p5js Editor)
-*/
-let writeMessage, readMessage;
-let readMessageButton, writeMessageButton;
-let writeMessageBox;
+let writeMessage, readMessage, writeMessageBox, cipherInputBox;
 let imageRead, imageWrite;
 let baseCol = 220;
-let messageLen = 160;
-let InterL, InterSB;
-
-function preload() {
-  interL = loadFont('Inter-Light.ttf');
-  interSB = loadFont('Inter-SemiBold.ttf');
-}
+let messageLen = 280; //default length
+let cypherInput;
+let cipher = [];
 
 function setup() {
-  createCanvas(600, 600);
-  pixelDensity(1);
-
-  // textFont(interL);
-  writeMessage = createFileInput(handleWrite);
-  writeMessage.position(50, 190);
-  writeMessage.style('color', color(240));
-  readMessage = createFileInput(handleRead);
-  readMessage.position(350, 190);
-  readMessage.style('color', color(30));
-  wFrame();
-}
-
-// —————————— Visual Layout —————————— 
-function wFrame() {
-
-  //Backgrounds
-  background(baseCol);
-  noStroke();
-  fill(240);
-  rect(0, 0, 300, height);
-  fill(30);
-  rect(300, 0, 300, height);
-
-  //Foreshadowing
-  blendMode(DIFFERENCE);
-  for (let i = 0; i < 150; i += 1) {
-    noStroke();
-    fill(255);
-    rect(random(width), random(0, 50), 3, 3);
-    rect(random(width), random(50, 100), 2, 2);
-    rect(random(width), random(100, 170), 1, 1);
-  }
-  for (let i = 0; i < 150; i += 1) {
-    noStroke();
-    fill(255);
-    rect(random(width), random(580, 600), 3, 3);
-    rect(random(width), random(550, 580), 2, 2);
-    rect(random(width), random(500, 550), 1, 1);
-  }
-  blendMode(BLEND);
-
-  textAlign(LEFT);
-  //Encrypt
-  fill(30);
-  textSize(32);
-  textFont(interSB);
-  text("Encrypt", 50, 110);
-
-  //Decrypt
-  fill(230);
-  textSize(32);
-  textFont(interSB);
-  text("Decrypt", 350, 110);
-}
-
-// —————————— Write Block —————————— 
-function handleWrite(file) {
-  print(file);
-  textSize(11);
-  fill(240);
-  noStroke();
-  rect(40, 200, 200, 40);
-  if (file.type === 'image') {
-    imageWrite = loadImage(file.data);
-    fill(30);
-    textFont(interL);
-    text("Upload Sucessful", 50, 230);
-    fill(20, 200, 20);
-    noStroke();
-    ellipse(35, 200, 12, 12);
+    noCanvas();
+    loadStrings('cypher.txt', readString);
+    writeMessage = createFileInput(handleWrite);
     writeMessageBox = createElement('textarea');
     writeMessageBox.elt.placeholder = 'write your message here (check length of message!)';
     writeMessageBox.attribute('rows', '9');
-    writeMessageBox.position(50, 260);
-    writeMessageBox.style('font-family', interL)
-    readMessageButton = createButton('Encrypt');
-    readMessageButton.style('cursor', 'pointer');
-    readMessageButton.style('background-color', color(30));
-    readMessageButton.style('color', color(240));
-    readMessageButton.style('border', 'none');
-    readMessageButton.style('font-family', 'Inter');
-    readMessageButton.position(50, 420);
-    readMessageButton.mousePressed(writeEngine);
-  } else {
-    imageWrite = null;
-    fill(200, 20, 20);
-    noStroke();
-    ellipse(35, 200, 12, 12);
-    fill(30);
-    textFont(interL);
-    text("Please upload an image!", 50, 230)
-  }
+    writeMessageBox.hide();
+    readMessageButton1 = createButton('Encrypt');
+    readMessageButton1.hide();
+    writeMessageBox.parent('encrypt');
+    readMessageButton1.parent('encrypt');
+    writeMessage.parent('encrypt');
+
+
+    readMessage = createFileInput(handleRead);
+    readMessageButton2 = createButton('Decrypt');
+    readMessageButton2.hide();
+    cipherInputBox = createElement('textarea');
+    cipherInputBox.elt.placeholder = 'write down the cipher';
+    //    cipherInput.attribute('rows', '1');
+    cipherInputBox.hide();
+    cipherInputBox.parent('decrypt');
+    readMessage.parent('decrypt');
+    readMessageButton2.parent('decrypt');
+
+    //  wFrame();
+}
+
+function readString(result) {
+    cypherInput = result[0];
+    cipher = split(cypherInput, '');
+    cipher = Dshuffle(cipher);
+    console.log(cipher);
+    //    console.log(cipher);
+    //    console.log(FYshuffle(cypher1));
+    //    console.log(Dshuffle(cypher1));
+
+}
+
+
+function handleWrite(file) {
+    print(file);
+
+    rect(40, 200, 200, 40);
+    if (file.type === 'image') {
+        text("Upload Sucessful", 50, 230);
+        imageWrite = loadImage(file.data);
+        writeMessageBox.show();
+        readMessageButton1.show();
+        readMessageButton1.mousePressed(writeEngine);
+    } else {
+        imageWrite = null;
+        text("Please upload an image!", 50, 230)
+    }
+}
+
+
+
+function handleRead(file) {
+    print(file);
+
+    if (file.type === 'image') {
+        imageRead = loadImage(file.data);
+
+        text("Upload Sucessful", 350, 230);
+        cipherInputBox.show();
+        readMessageButton2.show();
+        readMessageButton2.mousePressed(readEngine);
+    } else {
+        imageRead = null;
+        text("Please upload an image!", 350, 230);
+    }
 
 }
 
 function writeEngine() {
-  let inputMessage = writeMessageBox.value();
-  inputMessage = inputMessage.toLowerCase();
-  if (imageWrite) {
-    let messageArr = [];
-    let imageColArr = [];
-    imageWrite.loadPixels();
-    for (let x = 0; x < messageLen; x++) {
-      for (let y = 0; y < 1; y++) {
-        let index = (x + y * messageLen);
-        imageColArr[index] = imageWrite.get(x, y);
+    let inputMessage = writeMessageBox.value();
+    inputMessage = inputMessage.toLowerCase();
+    messageLen = inputMessage.length;
+    if (imageWrite) {
+        let messageArr = [];
+        let imageColArr = [];
+        imageWrite.loadPixels();
+        for (let x = 0; x < messageLen; x++) {
+            for (let y = 0; y < 1; y++) {
+                let index = (x + y * messageLen);
+                imageColArr[index] = imageWrite.get(x, y);
 
-        messageArr[index] = inputMessage.charAt(index);
-        let d;
-        for (var i = 0; i < cipher.length; i++) {
-          if (messageArr[index] == cipher.charAt(i)) {
-            d = i;
-            break;
-          } else {
-            d = 255;
-          }
+                messageArr[index] = inputMessage.charAt(index);
+                let d;
+                for (var i = 0; i < cipher.length; i++) {
+                    if (messageArr[index] == cipher[i]) {
+                        d = i;
+                        break;
+                    } else {
+                        d = 255;
+                    }
+                }
+                imageWrite.set(x, y, color(d, imageColArr[index][1], imageColArr[index][2]));
+            }
         }
-        imageWrite.set(x, y, color(d, imageColArr[index][1], imageColArr[index][2]));
-      }
+
+        imageWrite.updatePixels();
+        save(imageWrite);
+        var h = document.createElement("p") // Create a <h1> element
+        //        let message = join(cipher, '');
+        var t = document.createTextNode((join(cipher, ''))); // Create a text node
+        //        h.parent(t);
+        h.appendChild(t);
+        document.body.appendChild(h);
     }
-
-    imageWrite.updatePixels();
-    save(imageWrite);
-  }
-}
-
-
-// —————————— Read Block —————————— 
-function handleRead(file) {
-  print(file);
-  textSize(12);
-  fill(30);
-  noStroke();
-  rect(340, 200, 200, 40);
-  if (file.type === 'image') {
-    imageRead = loadImage(file.data);
-    fill(20, 200, 20);
-    noStroke();
-    ellipse(335, 200, 12, 12);
-    fill(230);
-    textFont(interL);
-    text("Upload Sucessful", 350, 230);
-    readMessageButton = createButton('Decrypt');
-    readMessageButton.position(350, 260);
-    readMessageButton.mousePressed(readEngine);
-  } else {
-    imageRead = null;
-    fill(200, 20, 20);
-    noStroke();
-    ellipse(335, 200, 12, 12);
-    fill(230);
-    textFont(interL);
-    text("Please upload an image!", 350, 230);
-  }
 
 }
 
 function readEngine() {
-  if (imageRead) {
-    let messageArr = [];
-    let showMessageArr = [];
-    for (let x = 0; x < messageLen; x++) {
-      for (let y = 0; y < 1; y++) {
-        let index = (x + y * messageLen);
-        messageArr[index] = imageRead.get(x, y);
-        if (messageArr[index][0] == 255) {
-          showMessageArr[index] = "";
-        } else {
-          showMessageArr[index] = cipher.charAt(messageArr[index][0]);
+    let cipherInput = cipherInputBox.value();
+    cipherInput = cipherInput.toLowerCase();
+    if (imageRead) {
+        if (cipherInput) {
+            let messageArr = [];
+            let showMessageArr = [];
+            for (let x = 0; x < messageLen; x++) {
+                for (let y = 0; y < 1; y++) {
+                    let index = (x + y * messageLen);
+                    messageArr[index] = imageRead.get(x, y);
+                    if (messageArr[index][0] == 255) {
+                        showMessageArr[index] = "";
+                    } else {
+                        showMessageArr[index] = cipherInput.charAt(messageArr[index][0]);
+                    }
+                }
+            }
+            text(join(showMessageArr, ''), 355, 320, 190, 170);
+            var h = document.createElement("p") // Create a <h1> element
+            var t = document.createTextNode((join(showMessageArr, ''))); // Create a text node
+            //        h.parent(t);
+            h.appendChild(t);
+            document.body.appendChild(h);
+            //            h.appendChild('decrypt');
         }
-      }
     }
-    fill(baseCol);
-    noStroke();
-    fill(30);
-    rect(350, 300, 250, 200);
-    fill(240);
-    text(join(showMessageArr, ''), 355, 320, 190, 170);
-  }
 }
 
+/*
+Attribution
+textarea: @lmccart (Github) @kanolab (p5js Editor)
+*/
 
-// —————————— Cipher —————————— 
-let cipher = 'abcdefghijklmnopqrstuvwxyz0123456789.,-?@ '; //255 chars only
+
+/* Cypher Shuffle Algorithms */
+
+function FYshuffle(array) { //Fisher-Yates (aka Knuth) Shuffle
+    //taken from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function Dshuffle(array) { // Durstenfeld shuffle
+    //taken from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
